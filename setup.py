fab9cf6b792340249983edca06172fc50ae955ef
William Benoit <benoi090@umn.edu>
2025-11-12 10:45:10 -0800

    Fixed pre-commit issues

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 import os
-from setuptools import setup, find_packages
+
+from setuptools import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-NAME = 'h5tree'
+
+NAME = "h5tree"
 DESCRIPTION = "tree for HDF5 files"
-URL = 'https://github.com/johnaparker/h5tree'
-EMAIL = 'japarker@uchicago.edu'
-AUTHOR = 'John Parker'
-KEYWORDS = 'hdf5 tree'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0'
-LICENSE = 'MIT'
+URL = "https://github.com/johnaparker/h5tree"
+EMAIL = "japarker@uchicago.edu"
+AUTHOR = "John Parker"
+KEYWORDS = "hdf5 tree"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "1.0"
+LICENSE = "MIT"
 
 REQUIRED = [
-    'h5py',
-    'termcolor',
+    "h5py",
+    "termcolor",
+    "pre-commit",
 ]
 
 setup(
@@ -27,14 +31,14 @@ setup(
     description=DESCRIPTION,
     license=LICENSE,
     keywords=KEYWORDS,
-    scripts=['bin/h5tree'],
+    scripts=["bin/h5tree"],
     url=URL,
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     install_requires=REQUIRED,
     python_requires=REQUIRES_PYTHON,
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
     ],
 )
